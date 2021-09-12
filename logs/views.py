690c3332201f693ee002d832e7d047ba66cf0799
Mmarcos01 <marie.marcos25@gmail.com>
2021-09-12 11:48:33 -0700

    feat: permissions

--- logs/views.py
+++ logs/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .serializers import LogSerializer
 from .models import Log
+from .permissions import IsOwnerOrReadOnly
 
 class LogList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Log.objects.all()
     serializer_class = LogSerializer
 
 class LogDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Log.objects.all()
     serializer_class = LogSerializer
